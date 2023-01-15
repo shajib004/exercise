227b30961b5595bc3a421a8011fb8299306cba65
Md Shariful Alam <shajib004@gmail.com>
2023-01-15 13:09:13 +0100

    upload modules

--- s3_reproducibility/exercise_files/pytorch_docker.py
+++ s3_reproducibility/exercise_files/pytorch_docker.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import torch
 
 if __name__ == "__main__":
     cuda = torch.cuda.is_available()
     print(f"CUDA enabled: {cuda}")
     if cuda:
-=======
-import torch
-
-if __name__ == "__main__":
-    cuda = torch.cuda.is_available()
-    print(f"CUDA enabled: {cuda}")
-    if cuda:
->>>>>>> 1d4db2f081cb7f024eed821f55ecde73b820a964
         print(f"Number of GPUs: {torch.cuda.device_count()}")
